15a67406828074f2628cece6b418a35c2255ee5f
TWRP Maintainer <maintainer@twrp.me>
2026-04-24 18:17:06 +0000

    cleanup: Remove unnecessary factory recovery files
    - Remove original TWRP GUI libs from factory recovery: libminuivivo.so, librecovery_ui.so
    - Remove unnecessary debug/log/wifi binaries from system/bin
    - Remove unnecessary WiFi/debug libraries from system/lib64
    - Remove factory recovery binaries: recovery, minadbd, init
    - device.mk: Keep the complete configuration for TWRP compilation
    All unnecessary files inherited from factory recovery have been removed,
    only required decryption (FBE), touch, adb and file system tools are kept.

--- device.mk
+++ device.mk
@@ -6,3 +6,37 @@
 #
 
 LOCAL_PATH := device/vivo/PD1936
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Qualcomm
+PRODUCT_USE_QCOM_COMMON := true
+
+# Inherit from our custom product configuration
+PRODUCT_PACKAGES += \
+    libion \
+    libfuse
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
+# QCOM Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.3-service.xiaomi
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
+# Keystore
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0
+
+# Copy fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/etc/recovery.fstab
