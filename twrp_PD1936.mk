fb987142902ab5561528fc3ec9cdeccc4ecf257a
hjiyu1965 <168966132+hjiyu1965@users.noreply.github.com>
2026-08-03 05:51:01 +0800

    Update twrp_PD1936.mk

--- twrp_PD1936.mk
+++ twrp_PD1936.mk
@@ -8,10 +8,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, device/qcom/twrp-common/Android.mk)
 # Inherit from PD1936 device
 $(call inherit-product, device/vivo/PD1936/device.mk)
 
